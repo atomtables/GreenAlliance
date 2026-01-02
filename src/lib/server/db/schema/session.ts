ecee130b7c0b667f7b1d1c538f33fb8abf985dfd
Adithiya Venkatakrishnan <adithiya.venkatakrishnan@atomtables.dev>
2026-01-02 17:59:50 -0500

    Revert "Reapply "split the schema into several files""
    This reverts commit e5c958bb915ae81609e6286fa0125cf2f857f506.

--- src/lib/server/db/schema/session.ts
+++
@@ -1,8 +0,0 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
-import { users } from './users';
-
-export const session = sqliteTable('session', {
-    id: text('id').primaryKey(),
-    userId: text('user_id').notNull().references(() => users.id),
-    expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
-});
