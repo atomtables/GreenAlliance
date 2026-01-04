99033202f185a24d6ef2bb3da6edeaad8fcf2374
Adithiya Venkatakrishnan <adithiya.venkatakrishnan@atomtables.dev>
2026-01-04 15:01:20 -0500

    feat: admi warning

--- src/routes/(protected)/home/+page.svelte
+++ src/routes/(protected)/home/+page.svelte
@@ -1,4 +1,16 @@
-<script lang="ts"></script>
+<script lang="ts">
+    import { Role } from "$lib/types/types";
+    import { onMount } from "svelte";
+    import { alert } from "$lib/components/Dialog.svelte";
+    
+    let {data} = $props();
+
+    onMount(() => {
+        if (data.user.role === Role.administrator) {
+            alert("Warning", "You are currently logged in as an administrator. Administrators have all permissions, regardless of whatever is set in the database. For this reason, please exercise caution while logged into this account. Do not use this account for anything other than configuration. If you require an account, please make one with the appropriate permissions.")
+        }
+    })
+</script>
 
 <div>
 
