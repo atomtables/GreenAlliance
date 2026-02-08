37706264390069b32b6c2763c976972d78cfc174
abominablepug <esnahoum@gmail.com>
2026-02-08 13:51:40 -0500

    edit endpoint created

---
+++ src/routes/api/meetings/[meetingId]/+server.ts
@@ -0,0 +1,59 @@
+import { Permission } from '$lib/types/types';
+import * as schema from "$lib/server/db/schema.js"
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from '@sveltejs/kit';
+import { db } from '$lib/server/db';
+import { eq } from 'drizzle-orm';
+
+export const GET: RequestHandler = async ({ params, locals }: any) => {
+	if (!locals?.user) return error(401, "Unauthorized");
+
+	const { meetingId } = params;
+	if (!meetingId) return error(400, "Missing required parameter: meetingId");
+
+	try {
+		const meeting = await db.query.schema.meetings.findFirst({
+			where: eq(schema.meetings.id, meetingId)
+		});
+
+		if (!meeting) return error(404, "Meeting not found");
+
+		if (!meeting.members.includes(locals.user.id) && !locals.user.permissions?.includes?.(Permission.calendar_moderate)) {
+			return error(403, "Access denied");
+		}
+
+		return json({ success: true, data: meeting }, { status: 200 });
+
+	} catch (e: any) {
+		if (e.name === "HttpError") throw e;
+		console.log(e);
+		return error(500, e.message || "Internal server error");
+	}
+}
+
+export const PATCH: RequestHandler = async ({ request, params, locals }: any) => {
+	if (!locals?.user?.permissions?.includes?.(Permission.calendar_moderate)) return error(403, "Access denied.");
+
+	const { meetingId } = params;
+	if (!meetingId) return error(400, "Missing required parameter: meetingId");
+
+	let { title, description, date, applicableSubteams, members } = await request.json();
+	if (!title || !date || isNaN(new Date(date).getTime())) return error(400, "Please fill out all necessary fields.");
+
+	try {
+		await db.update(schema.meetings).set({
+			title,
+			description: description || null,
+			dateOf: new Date(date),
+			subteams: applicableSubteams || [],
+			members: members || []
+		} as any).where(eq(schema.meetings.id, meetingId));
+
+		return json({ success: true, data: {} }, { status: 200 });
+
+	} catch (e: any) {
+		if (e.name === "HttpError") throw e;
+		console.log(e);
+		return error(500, e.message || "Internal server error");
+	}
+}
